27d9df0390a60a06f0f0c818a8fa23e98852fd9c
MelvinPereira <pereiramelvin78@gmail.com>
2022-02-12 13:50:01 -0600

    Initial commit

--- app/src/main/java/com/example/learningletters/MainActivity.java
+++ app/src/main/java/com/example/learningletters/MainActivity.java
@@ -5,16 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public Button ActivityVocal;
     public Button ActivityAlfabeto;
     public TextView titul;
     public ImageView botonatras;
+    int contador;
+
+    @Override
+    public void onBackPressed() {
+        if(contador==0)
+        {
+            Toast.makeText(getApplicationContext(),"Presione de Nuevo Para salir.",Toast.LENGTH_LONG).show();
+            contador++;
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                contador=0;
+            }
+        }.start();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this,MainActivityVocales.class);
                 startActivity(i);
-                finish();
             }
         });
         //abecedario
@@ -38,14 +68,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this,MainActivityAlfabeto.class);
                 startActivity(i);
-                finish();
-
-
             }
         });
 
         titul.setText("LearningLetters");
         botonatras.setVisibility(View.INVISIBLE);
+
 
     }
 }
